6f00f7dd9bfa75f3ac2814969772ae79c26f203b
RSaitta <flycords@gmail.com>
2018-10-14 22:52:38 -0700

    added persistent login function

--- desktop_app.py
+++ desktop_app.py
@@ -1,38 +1,59 @@
 import pyforms
 from pyforms.basewidget import BaseWidget
-from pyforms.controls import ControlEmptyWidget, ControlText, ControlButton
+from pyforms.controls import ControlEmptyWidget, ControlText, ControlButton, ControlLabel
 from DB_Actions import db_connect
-from ControlPasswordText import ControlPasswordText
 from LoginWindow import loginWidget
 from Notifications_View import NotificationsWidget
+from datetime import datetime
+from time import sleep
 
-
+delta_hour =0
 
 class BlackInkBE(BaseWidget):
     def __init__(self):
         super(BlackInkBE, self).__init__('BlackInk Backend')
-        self._test = ControlText('logged in user', readonly=True)
-        #self._test.readonly = True
+        self._display_name = ControlLabel('')
         self._user = None
         self._connection = db_connect()
         self._panel = ControlEmptyWidget()
-        self.login()
+        #self._formset= { ('||','_display_name', '||'), ('_panel')
+        #}
+        self._login()
+        #self._persist_login()
 
 
-    def login(self):
+    def _login(self):
         logwin = loginWidget()
         logwin.parent = self
         self._panel.value = logwin
+        self._login_time = datetime.now().hour
 
-    def Notifications(self):
+    def _logout(self):
+        self._user = None
+        self._connection = None
+        self._login()
+
+    def _Notifications(self):
         notwin = NotificationsWidget(self._user, self._connection)
         notwin.parent=self
         self._panel.value =notwin
 
     def loadUser(self, user):
         self._user = user
-        self._test.value = self._user['email'].split('@')[0]
-        self.Notifications()
+        self._display_name.value = self._user['email'].split('@')[0]
+        #self._persist_login()
+        self._Notifications()
+
+    def persist_login(self):
+        cur_time = datetime.now().hour
+        if cur_time - self._login_time > 0.9:
+            try:
+                self._user = self._connection['Auth'].refresh(user['refreshToken'])
+            except:
+                self._login()
+
+
+
 
 
 
