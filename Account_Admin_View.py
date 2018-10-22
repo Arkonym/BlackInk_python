c25195a79f568199042f0127c1fbc2f14cd3c607
RSaitta <flycords@gmail.com>
2018-10-21 22:51:34 -0700

    added persistent user object list

--- Account_Admin_View.py
+++ Account_Admin_View.py
@@ -15,16 +15,20 @@ class AdminWidget(BaseWidget):
         self._userList.readonly=True
         self._userList.cell_double_clicked_event = self.__onSelect
         self._userList.horizontal_headers=['Name', 'Email']
-        user_pull= pull_users(self._admin, self._connection)
-        if user_pull!=[]:
-            for i in user_pull:
-                self._userList.__add__(pull_list[i]['name'], pull_list[i]['email'])
+        self._user_pull= pull_users(self._admin, self._connection)
+        if self._user_pull!=[]:
+            for i in self._user_pull:
+                self._userList.__add__([self._user_pull[i]['name'], self._user_pull[i]['email']])
 
     def __onSelect(self, row, column):
         if self.parent!=None: self.parent.persist_login()
         name = self._userList.get_value(0, row)
         email  = self._userList.get_value(1, row)
-        win = UserWidget(self._admin, self._connection, email, name)
+        for i in self._user_pull:
+            if self._user_pull[i]['email'] == email:
+                permissions= self._user_pull[i]['permissions']
+                services = self._user_pull[i]['services']
+        win = UserWidget(self._admin, self._connection, email, name, permissions, services)
         win.parent = self
         win.show()
 
