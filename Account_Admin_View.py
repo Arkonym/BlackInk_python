d31226afee5090be644202de217dc9e7e52c5fd0
RSaitta <flycords@gmail.com>
2018-11-17 16:33:11 -0800

    redesigning account class and view implementation

--- Account_Admin_View.py
+++ Account_Admin_View.py
@@ -16,9 +16,9 @@ class AdminWidget(BaseWidget):
         self._userList.cell_double_clicked_event = self.__onSelect
         self._userList.horizontal_headers=['Name', 'Email']
         self._user_pull= pull_users(self._admin, self._connection)
-        if self._user_pull!=[]:
-            for i in self._user_pull:
-                self._userList.__add__([self._user_pull[i]['name'], self._user_pull[i]['email']])
+        if self._user_pull!=None:
+            for user in self._user_pull:
+                self._userList.__add__([self._user_pull[user]['name'], self._user_pull[user]['email']])
 
     def __onSelect(self, row, column):
         if self.parent!=None: self.parent.persist_login()
@@ -26,11 +26,21 @@ class AdminWidget(BaseWidget):
         email  = self._userList.get_value(1, row)
         for i in self._user_pull:
             if self._user_pull[i]['email'] == email:
-                permission= self._user_pull[i]['permissions']
-                services = self._user_pull[i]['services']
-        win = UserWidget(self._admin, self._connection, email, name, permissions, services)
+                uid = i
+                try:
+                    services = self._user_pull[i]['services']
+                except:
+                    services = {"notifications" : False, "analysis": False, "newsletter" : False}
+        win = UserWidget(self._admin, self._connection, uid, email, name, services)
         win.parent = self
         win.show()
+
+    def update_user(account):
+        if account.changes[0]:
+            connection['Database'].child("users").child(account._uid).child("name").update(account.name)
+        if account.changes[1]:
+            connection['Database'].child("users").child(account._uid).child("services").update(account.services)
+
 
 if __name__== "__main__":
     pyforms.start_app(AdminWidget, geometry=(400, 400, 600, 600))
--- Account_View.py
+++ Account_View.py
@@ -6,11 +6,13 @@ from Error_Windows import ErrorWin, close_win
 from DB_Actions import pull_users
 
 class UserWidget(User, BaseWidget):
-    def __init__(self, admin=None, connection=None, email='', name='', services=[]):
+    def __init__(self, admin=None, connection=None, uid='', email='', name='', services={}):
         User.__init__(self, email, name, '', services)
         BaseWidget.__init__(self, 'User')
+        self._uid = uid
         self._admin = admin
         self._connection = connection
+        self.changes = [] #0 is name change, 1 is services change
 
         self._editBtn = ControlButton('Edit')
         self._editBtn.value = self._edit
@@ -25,21 +27,34 @@ class UserWidget(User, BaseWidget):
         if name!='':
             self._name_field = name
 
+        self._services = services
         self._services_field = ControlList('Services')
         self._services_field.readonly= True
         self._services_field.horizontal_headers = ['Service', 'Value']
-        self._services_field.cell_double_clicked_event = None
-        if services!=[]:
+        self._services_field.cell_double_clicked_event = self._toggle
+        if services!={}:
             for i in services:
-                self._services_field.__add__([services[i]['service'], services[i]['value']])
+                self._services_field.__add__([i, services[i]])
 
 
     def _edit(self):
         pass
     def _save(self):
-        pass
+        if self.parent!=None:
+            self.name = self._name_field.value
+            self.email = self._email_field.value
+            self.services = self._services
+            self.parent.update_user(self)
 
     def _toggle(self, row, column):
-        pass
+        val = self._services_field.get_value(column, row)
+        if val == True:
+            self._services_field.set_value(column, row, False)
+            self._services[self._services_field..get_value(0, row)] = False
+        else:
+            self._services_field.set_value(column, row,True)
+            self._services[self._services_field..get_value(0, row)] = True
+        self.changes[1] =True
+
 
 if __name__== "__main__": pyforms.start_app(UserWidget, geometry=(400, 400, 600, 600))
