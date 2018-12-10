acb6360905cf7d43d53302fa9e768674a2abf9b3
RSaitta <rsaitta@mail.csuchico.edu>
2018-12-09 22:40:37 -0800

    implemented conditional form fields and updated edit function to reflect this change

--- Account_View.py
+++ Account_View.py
@@ -1,22 +1,29 @@
 import pyforms
 from pyforms.basewidget import BaseWidget
-from pyforms.controls import ControlText, ControlButton, ControlList
+from pyforms.controls import ControlText, ControlButton, ControlList, ControlLabel
 from User import User
 from Error_Windows import ErrorWin, close_win
 from DB_Actions import pull_users
 
 class UserWidget(User, BaseWidget):
-    def __init__(self, admin=None, connection=None, uid='', email='', name='', services={}):
-        User.__init__(self, email, name, '', services)
+    def __init__(self, admin=None, connection=None, flag='', uid='', email='', name='', services={}):
+        super(UserWidget, self).__init__(uid, email, name, services)
         BaseWidget.__init__(self, 'User')
-        self._uid = uid
         self._admin = admin
         self._connection = connection
         self.changes = [] #0 is name change, 1 is services change
+        if flag=='edit':
+            self._editBtn = ControlButton('Edit')
+            self._editBtn.value = self._edit
+        elif flag=='new':
+            self._ediBtn = ControlButton('Save')
+        else:
+            self._closeBtn= ControlButton('Close')
+            self._closeBtn.value = self._close
 
-        self._editBtn = ControlButton('Edit')
-        self._editBtn.value = self._edit
-
+        if uid!='':
+            self._uid_field = ControlText('User_Key')
+            self._uid_field.value= uid
         self._email_field = ControlText('Email')
         self._email_field.readonly = True
         if email!='':
@@ -38,7 +45,7 @@ class UserWidget(User, BaseWidget):
 
 
     def _edit(self):
-        self._editBtn = ControlButton('Save')
+        self._editBtn.label = 'Save'
         self._editBtn.value = self._save
         self._email_field.readonly = False
         self._name_field.readonly = False
@@ -50,7 +57,12 @@ class UserWidget(User, BaseWidget):
             self.name = self._name_field.value
             self.email = self._email_field.value
             self.services = self._services
-            self.parent.update_user(self)
+            self.uid = self._uid_field.value
+            if flag=='edit':
+                self.parent._update_user(self)
+            elif flag =='new':
+                self.parent._add_User(self)
+        self._close()
 
     def _toggle(self, row, column):
         val = self._services_field.get_value(column, row)
@@ -61,6 +73,9 @@ class UserWidget(User, BaseWidget):
             self._services_field.set_value(column, row,True)
             self._services[self._services_field.get_value(0, row)] = True
         self.changes[1] =True
+
+    def _close(self):
+        close_win(self)
 
 
 if __name__== "__main__": pyforms.start_app(UserWidget, geometry=(400, 400, 600, 600))
